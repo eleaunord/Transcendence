0273821fa9f7d59ecaa9ac7187910e0e7c63b0c3
Rime YOUNSSI <ryounssi@paul-f3Br3s3.clusters.42paris.fr>
2025-05-06 13:13:57 +0200

    Project complete - final version

--- backend/src/db/migrations.ts
+++ backend/src/db/migrations.ts
@@ -10,11 +10,8 @@ function safeAlter(column: string, type: string) {
     console.log(` ==== Added column '${column}' to users table ==== `);
   }
 }
-<<<<<<< HEAD
-=======
 
 // --- add new vairables in DB --- \\
->>>>>>> 0205_merge_front_back
 
 // Fonction principale qui gère toutes les migrations
 async function migrate() {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,20 @@
-<<<<<<< HEAD
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import { authRoutes } from './routes/auth';
 import { meRoutes } from './routes/me';
 import { themeRoutes } from './routes/theme';
 import { matchRoutes } from './routes/match';
-=======
-// ========== Imports ==========
-import Fastify from 'fastify';
-import cors from '@fastify/cors'; // DONT REMOVE :added to solve "CORS (Cross-Origin Resource Sharing)" problem
-import { authRoutes } from './routes/auth';
-import { meRoutes } from './routes/me';
-import { themeRoutes } from './routes/theme';
->>>>>>> 0205_merge_front_back
 import './db/migrations';
 
 async function main() {
   const app = Fastify();
 
-<<<<<<< HEAD
   // Active CORS
-  await app.register(cors, {
-    origin: '*',
-  });
+  // await app.register(cors, {
+  //   origin: '*',
+  // });
 
   // Parseur JSON
-=======
->>>>>>> 0205_merge_front_back
   app.addContentTypeParser('application/json', { parseAs: 'string' }, (req, body, done) => {
     try {
       const json = JSON.parse(body as string);
@@ -36,7 +24,6 @@ async function main() {
     }
   });
 
-<<<<<<< HEAD
   // Enregistre les routes
   authRoutes(app);
   meRoutes(app);
@@ -47,7 +34,6 @@ async function main() {
     return { message: 'Backend is running' };
   });
 
-=======
   // register CORS
   await app.register(cors, {
     origin: 'http://localhost:8080',
@@ -59,21 +45,11 @@ async function main() {
   await app.register(meRoutes, { prefix: '/api' });
   await app.register(themeRoutes, { prefix: '/api' });
 
->>>>>>> 0205_merge_front_back
   const PORT = parseInt(process.env.PORT || '3001');
   const HOST = process.env.HOST || '0.0.0.0';
 
   app.listen({ port: PORT, host: HOST }, () => {
-<<<<<<< HEAD
     console.log(`✅ Backend running on http://${HOST}:${PORT}`);
-  });
-}
-
-main(); // lance l'appli
-
-
-=======
-    console.log(`Backend is running on http://${HOST}:${PORT}`);
   });
 }
 
@@ -82,4 +58,3 @@ main().catch(err => {
   console.error('Failed to start server:', err);
   process.exit(1);
 });
->>>>>>> 0205_merge_front_back
